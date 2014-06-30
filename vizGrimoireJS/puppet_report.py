12beffe96c76daa58127ea3836360eeeb1b154a3
Alvaro del Castillo <acs@bitergia.com>
2014-06-30 11:58:19 +0200

    PYTHONPATH and ENV vars are now added from python code.

--- vizGrimoireJS/puppet_report.py
+++ vizGrimoireJS/puppet_report.py
@@ -22,20 +22,11 @@
 ##   Daniel Izquierdo-Cortazar <dizquierdo@bitergia.com>
 ##   Luis Cañas-Díaz <lcanas@bitergia.com>
 
-from optparse import OptionParser
-from os import listdir
-from os.path import isfile, join
 import imp, inspect
-
-from metrics import Metrics
-from query_builder import DSQuery, SCMQuery, QAForumsQuery, MLSQuery
-from metrics_filter import MetricFilters
-import scm_metrics as scm
-import qaforums_metrics as qa
-import mls_metrics as mls
-from GrimoireUtils import createJSON
-from GrimoireSQL import SetDBChannel
-
+from optparse import OptionParser
+from os import listdir, path, environ
+from os.path import isfile, join
+import sys
 
 def read_options():
 
@@ -266,8 +257,30 @@ def createCSV(data, filepath, skip_fields = []):
     fd.close()
     print "CSV file generated at: %s" % (filepath)
 
+def init_env():
+    grimoirelib = path.join("..","vizgrimoire")
+    metricslib = path.join("..","vizgrimoire","metrics")
+    studieslib = path.join("..","vizgrimoire","analysis")
+    alchemy = path.join("..","grimoirelib_alch")
+    for dir in [grimoirelib,metricslib,studieslib,alchemy]:
+        sys.path.append(dir)
+
+    # env vars for R
+    environ["LANG"] = ""
+    environ["R_LIBS"] = "../../r-lib"
 
 if __name__ == '__main__':
+
+    init_env()
+
+    from metrics import Metrics
+    from query_builder import DSQuery, SCMQuery, QAForumsQuery, MLSQuery
+    from metrics_filter import MetricFilters
+    import scm_metrics as scm
+    import qaforums_metrics as qa
+    import mls_metrics as mls
+    from GrimoireUtils import createJSON
+    from GrimoireSQL import SetDBChannel
 
     # parse options
     opts = read_options()    
